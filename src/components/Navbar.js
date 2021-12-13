cf2de5906fc7488edf71ecb99c73f0d55d5d050e
zhangir Ospan <ospanzhangir2005@gmail.com>
2021-12-13 11:10:12 +0600

    modal component unstyled

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,9 +1,37 @@
 import React, {useEffect} from 'react'
 import logo from '../img/Logo Ulive.png'
 import Button from './Button.js';
+import Modal from 'react-modal';
 import "./Navbar.css";
 
+const customStyles = {
+  content : {
+    top                   : '50%',
+    left                  : '50%',
+    right                 : 'auto',
+    bottom                : 'auto',
+    marginRight           : '-50%',
+    transform             : 'translate(-50%, -50%)'
+  }
+};
+
+Modal.setAppElement('#root');
+
 function Navbar() {
+  let subtitle;
+  const [modalIsOpen,setIsOpen] = React.useState(false);
+  function openModal() {
+    setIsOpen(true);
+  }
+
+  function afterOpenModal() {
+    // references are now sync'd and can be accessed.
+    subtitle.style.color = '#f00';
+  }
+
+  function closeModal(){
+    setIsOpen(false);
+  }
   const [scrolled, setScrolled]=React.useState(false);
   const handleScroll=() => {
     const offset=window.scrollY;
@@ -31,9 +59,26 @@ function Navbar() {
             <li className="navbar-list-element"><a className="navbar-list-link" href="/">Вакансии</a></li>
             <li className="navbar-list-element"><a className="navbar-list-link" href="/">FAQ</a></li>
             <li className="navbar-list-element"><a className="navbar-list-link" href="/">Психологи</a></li>
-            <li className="navbar-list-element "><Button buttonSize="btn-medium">Записаться на сеанс</Button></li>
+            <li className="navbar-list-element "><Button onClick={openModal} buttonSize="btn-medium">Записаться на сеанс</Button></li>
           </ul>
-          
+          <Modal
+            isOpen={modalIsOpen}
+            onAfterOpen={afterOpenModal}
+            onRequestClose={closeModal}
+            style={customStyles}
+            contentLabel="Example Modal">
+
+            <h2 ref={_subtitle => (subtitle = _subtitle)}>Hello</h2>
+            <button onClick={closeModal}>close</button>
+            <div>I am a modal</div>
+            <form>
+              <input />
+              <button>tab navigation</button>
+              <button>stays</button>
+              <button>inside</button>
+              <button>the modal</button>
+            </form>
+          </Modal>
         </nav>
       </header>
     </>
--- src/components/QuestionsSection.js
+++ src/components/QuestionsSection.js
@@ -15,56 +15,58 @@ function Questions() {
   // }
   return (
     <div className="questions-container">
-      <p className="questions-paragraph">Остались вопросы? Напишите нам, мы поможем</p>
-      <div className="social-medias-wrapper">
-        <div className="social-media">
-          <a className="social-media-text" href="/">
-            <img className="social-media-icon" src={telegram} alt="telegram"/>
-            <p>Telegram</p>
-          </a>
-        </div>
-        <div className="social-media">
-          <a className="social-media-text" href="/">
-            <img className="social-media-icon" src={whatsapp} alt="whatsapp"/>
-            <p>Whatsapp</p>
-          </a>
-        </div>
-        <div className="social-media">
-          <a className="social-media-text" href="/">
-            <img className="social-media-icon" src={messenger} alt="messenger"/>
-            <p>Messenger</p>
-          </a>
+      <div className="questions-wrapper">
+        <p className="questions-paragraph">Остались вопросы? Напишите нам, мы поможем</p>
+        <div className="social-medias-wrapper">
+          <div className="social-media">
+            <a className="social-media-text" href="/">
+              <img className="social-media-icon" src={telegram} alt="telegram"/>
+              <p>Telegram</p>
+            </a>
+          </div>
+          <div className="social-media">
+            <a className="social-media-text" href="/">
+              <img className="social-media-icon" src={whatsapp} alt="whatsapp"/>
+              <p>Whatsapp</p>
+            </a>
+          </div>
+          <div className="social-media">
+            <a className="social-media-text" href="/">
+              <img className="social-media-icon" src={messenger} alt="messenger"/>
+              <p>Messenger</p>
+            </a>
+          </div>
         </div>
       </div>
       <h2 className="main-heading faq-heading">Часто задаваемые вопросы</h2>
       <ul className="faq-list">
-        <li className="faq-list-item"><Accordion className="faq-question-button">
-        <AccordionSummary style={{border: 'none', padding: '0px', minHeight: '0px'}}
-          aria-controls="panel1a-content"
-        >
-          <Typography className="question-text">Эффективно ли онлайн консультирование?</Typography>
-        </AccordionSummary>
-        <AccordionDetails className="answer-wrapper">
-          <Typography className="answer-text">
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse
-            malesuada lacus ex, sit amet blandit leo lobortis eget.
-          </Typography>
-        </AccordionDetails>
-      </Accordion></li>
         <li className="faq-list-item">
-        <Accordion className="faq-question-button">
-        <AccordionSummary style={{border: 'none', padding: '0px', minHeight: '0px'}}
-          aria-controls="panel1a-content"
-        >
-          <Typography className="question-text">Эффективно ли онлайн консультирование?</Typography>
-        </AccordionSummary>
-        <AccordionDetails className="answer-wrapper">
-          <Typography className="answer-text">
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse
-            malesuada lacus ex, sit amet blandit leo lobortis eget.
-          </Typography>
-        </AccordionDetails>
-      </Accordion>
+          <Accordion className="faq-question-button">
+            <AccordionSummary style={{border: 'none', padding: '0px', minHeight: '0px'}}
+            aria-controls="panel1a-content">
+              <Typography className="question-text">Эффективно ли онлайн консультирование?</Typography>
+            </AccordionSummary>
+            <AccordionDetails className="answer-wrapper">
+              <Typography className="answer-text">
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse
+                malesuada lacus ex, sit amet blandit leo lobortis eget.
+              </Typography>
+            </AccordionDetails>
+          </Accordion>
+        </li>
+        <li className="faq-list-item">
+          <Accordion className="faq-question-button">
+            <AccordionSummary style={{border: 'none', padding: '0px', minHeight: '0px'}}
+            aria-controls="panel1a-content">
+              <Typography className="question-text">Эффективно ли онлайн консультирование?</Typography>
+            </AccordionSummary>
+            <AccordionDetails className="answer-wrapper">
+              <Typography className="answer-text">
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse
+                malesuada lacus ex, sit amet blandit leo lobortis eget.
+              </Typography>
+            </AccordionDetails>
+          </Accordion>
         </li>
         <li className="faq-list-item">
           <Accordion className="faq-question-button">
